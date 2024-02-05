1aab13861ea9520f33aaed1c4281427697316fa0
KARTIK PANDEY <kartikpandey07@KARTIKs-MacBook-Air.local>
2024-02-06 04:35:09 +0530

    Explore changed

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PostForm from '../components/forms/PostForm'
 import PostAddIcon from '@mui/icons-material/PostAdd';
 
+
 const CreatePost = () => {
   return (
     <div className="flex flex-1 bg-primary-500">
